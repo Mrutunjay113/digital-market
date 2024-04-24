2503b5fc9a278622dfdc0e2f9865219807f1ca47
Mrutunjay113 <mrutunjay765@gmail.com>
2024-04-24 19:21:16 +0530

    Fix formatting in payload-utils.ts

--- src/lib/payload-utils.ts
+++ src/lib/payload-utils.ts
@@ -1,25 +1,25 @@
-import { User } from '../payload-types'
-import { ReadonlyRequestCookies } from 'next/dist/server/web/spec-extension/adapters/request-cookies'
-import { NextRequest } from 'next/server'
+import { User } from "../payload-types";
+import { ReadonlyRequestCookies } from "next/dist/server/web/spec-extension/adapters/request-cookies";
+import { NextRequest } from "next/server";
 
 export const getServerSideUser = async (
-  cookies: NextRequest['cookies'] | ReadonlyRequestCookies
+  cookies: NextRequest["cookies"] | ReadonlyRequestCookies
 ) => {
-  const token = cookies.get('payload-token')?.value
+  const token = cookies.get("payload-token")?.value;
+  const serverUrl =
+    process.env.NEXT_PUBLIC_SERVER_URL ||
+    "https://digital-market-olive.vercel.app";
 
-  const meRes = await fetch(
-    `${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/me`,
-    {
-      headers: {
-        Authorization: `JWT ${token}`,
-      },
-    }
-  )
+  const meRes = await fetch(`${serverUrl}/api/users/me`, {
+    headers: {
+      Authorization: `JWT ${token}`,
+    },
+  });
+  console.log(meRes);
 
   const { user } = (await meRes.json()) as {
-    user: User | null
-  }
+    user: User | null;
+  };
 
-  
-  return { user }
-}
+  return { user };
+};
